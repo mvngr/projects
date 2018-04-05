1fb92182c1af17b60dffd6980a872b3beb7c8982
Mike <mvngr@niuitmo.ru>
2018-04-06 00:57:40 +0300

    1st commit

---
+++ flowers_landing/test.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="https://fonts.googleapis.com/css?family=Pacifico|Rubik:500,700,900" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css?1">
+</head>
+<body>
+	<div id="content">
+		<div class="schedule">
+			<div class="right">
+				<p>ПН-ПТ: <span>10:00 - 21:00</span></p>
+				<p>СБ-ВС: <span>11:00 - 22:00</span></p>
+				<p>Телефон: <span><a href="tel:+78142332211">+7(814)-233-22-11</a></span></p>
+				<p>Адрес: <span><a href="https://goo.gl/maps/yYxeqhgfSAA2" target="_blank">Ленинградский просп., 65a</a></span></p>
+				<div class="fix"></div>
+			</div>
+		</div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est adipisci, soluta odit, voluptates, aliquam possimus magni sunt expedita cum illum, voluptas. Vero exercitationem, incidunt minima iure, minus fugit inventore maiores.</p>
+		<div class="help">
+			<p>У вас есть вопросы? Мы готовы ответить!</p>
+			<a href="/help/">Задать вопрос</a>
+		</div>
+		<p>Гегель в своей философской системе принимал те же три ступени в развитии познающей силы, но устранял из этого процесса всякий вид произвольной деятельности, считая весь процесс необходимым движением от одной ступени развития к другой – от бытия в себе чрез бытие вне себя к бытию в себе и для себя (идея, природа, дух). Необходимый процесс саморазвития совершается, по Гегелю, в чистом или абсолютном разуме (идее), вследствие чего разум (мышление) оказывается единственным и действительно сущим, а все действительное необходимо разумным. Разум в этой системе есть, следовательно, единственная субстанция, но не реальная, а чисто идеальная и логическая (поэтому философию Гегеля часто именуют панлогизмом). Превратить эту субстанцию в субъект, т. е. первоначальный бессознательный разум в самостоятельный, в дух и даже в абсолютный Дух, так как субстанция есть абсолютный разум, составляет задачу мирового процесса. Выхождение субстанции из её первоначального вида существования, как логической идеи, в инобытие, как природы, и заключительное понимание себя самой, как единого и истинно действительного, понимание, что такое абсолютная идея, какова она есть в развитом своем существе, составляет ступени мирового процесса.</p>
+		<div class="quote">А как этот текст смотрится на русском? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla voluptatum magni, facilis nobis expedita, eveniet et iure laborum maiores officia, deleniti impedit dolorem alias quas deserunt minus necessitatibus. Earum, voluptates.
+		</div>
+		<div class="links">
+			<a id="fb" href="#fab"><p>FB / my_link</p></a>
+			<a id="inst" href="#instagram"><p>Instagram / my_link</p></a>
+			<a id="fb" href="#vk"><p>VK / my_different_link</p></a>
+		</div>
+		<div class="catalog">
+			<div class="item" id="item55" style="background-image: url(https://floristxpress.com/blog/wp-content/uploads/2016/09/Red-Roses-beautiful-Arrangment.jpg);">
+				<div class="cost">
+					<span class="old">800</span>
+					<span class="new">600</span>
+				</div>
+			</div>
+			<div class="item" id="item55" style="background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy5qEoNTi60tt0v1mh3Pp1gYd8pZqNE3lb-Cdo4aqcjvFuaxY1A);">
+				<div class="cost">
+					<span class="old">160</span>
+					<span class="new">120</span>
+				</div>
+			</div>
+			<div class="item" id="item55" style="background-image: url(https://krokus73.ru/wp-content/uploads/2017/06/73665099.jpg);">
+				<div class="cost">
+					<span class="old">2000</span>
+					<span class="new">950</span>
+				</div>
+			</div>
+			<div class="item" id="item55" style="background-image: url(https://hopeflowers.ru/wa-data/public/shop/products/85/01/185/images/359/359.970.jpg);">
+				<div class="cost">
+					<span class="old">35000</span>
+					<span class="new">15000</span>
+				</div>
+
+			</div>
+			<div class="item" id="item55" style="background-image: url(https://flor2u.ru/images/uploads/conversion/9f6/9f696f0cbbbc546fe0d67d5bea2b7454/flor.detail.gallery.jpg);">
+				<div class="cost">
+					<span class="old">5800</span>
+					<span class="new">4600</span>
+				</div>
+				<div class="unaviable"></div>
+			</div>
+			<div class="item" id="item55" style="background-image: url(https://king.vl.ru/assets/images/products/61/356x400/six-thousand-five-hundred-thirty-seven.jpg);">
+				<div class="cost">
+					<span class="old">1000</span>
+					<span class="new">700</span>
+				</div>
+			</div>
+			<div class="item" id="item55" style="background-image: url(https://rastenievod.com/wp-content/uploads/2017/03/6-11.jpg);">
+				<div class="cost">
+					<span class="old">500</span>
+					<span class="new">300</span>
+				</div>
+			</div>
+			<div class="item" id="item55" style="background-image: url(http://www.obradoval.ru/resources/fotogr/2017_04/7ea73da57a6c7a0_orig.jpg);">
+				<div class="cost">
+					<span class="old">1300</span>
+					<span class="new">750</span>
+				</div>
+			</div>
+			<div class="item" id="item55" style="background-image: url(http://billionnews.ru/uploads/posts/2016-12/1482776055_6.jpg);">
+				<div class="cost">
+					<span class="old">900</span>
+					<span class="new">650</span>
+				</div>
+			</div>
+			<div class="fix"></div>
+		</div>
+	</div>
+</body>
+</html>
